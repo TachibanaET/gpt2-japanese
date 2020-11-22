17ab7d3167e7b75cad784b9057e080b80ea9c40d
ENFU.G <guoenfu321@gmail.com>
2020-11-22 23:46:37 +0900

    add gpt-2 small & medium model (pytorch ver)

--- fine-tune.py
+++ fine-tune.py
@@ -13,6 +13,7 @@ from encode_bpe import BPEEncoder_ja
 import torch
 import torch.nn.functional as F
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='gpt2ja-medium')
@@ -35,22 +36,12 @@ with open('ja-bpe-origin.txt') as f:
 with open('emoji.json') as f:
     emoji = json.loads(f.read())
 
-# enc = BPEEncoder_ja(bpe, emoji)
-# n_vocab = len(enc)
-# print('n_vocab = ', n_vocab)
-
-# def generate_one(sess, output):
-#     generated = ''
-#     pre_text = args.context if len(args.context)>0 else '<|endoftext|>'
-#     while True:
-#         context_tokens = enc.encode(pre_text)
-
 class fine_tune_test:
   def __init__(self):
     self.temperature = 1
     self.top_k       = 40
     self.top_p       = 0
-    self.enc         = BPEEncoder_ja(bpe_origin, emoji)
+    self.enc         = BPEEncoder_ja(bpe, emoji)
     self.model       = 'gpt2ja-medium'
     self.hparams = HParams(**{
       "n_vocab": len(self.enc),
@@ -61,65 +52,7 @@ class fine_tune_test:
     })
     self.length      = self.hparams.n_ctx // 2
 
-  def generate_one(self, sess, output):
-    generated = ''
-    pre_text = '<|endoftext|>'
-    while True:
-        context_tokens = self.enc.encode(pre_text)
-        if len(context_tokens) > self.length:
-            context_tokens = context_tokens[-self.length:]
-        out = sess.run(output, feed_dict={
-            self.context: [context_tokens]
-        })[:,len(context_tokens):]
-        swd = self.enc.decode(out[0])
-        last = False
-        if '<|endoftext|>' in swd:
-            swd = swd.split('<|endoftext|>')[0]
-            last = True
-        if len(swd) > 0:
-            generated += swd
-
-        if last or len(generated) > args.max_length:
-            if len(generated) > 0:
-                return generated[:args.max_length]
-        else:
-            pre_text = generated[-256:]
-
-  def fine_tune(self):
-    # pre_text = '<COMPANY>99999<LABEL>良い点<CATEGORY>給与水準<KEYWORDS>111 123 133'
-    # encode_result = self.enc.encode(pre_text)
-    # print(f'encode result = {encode_result}')
-    config = tf.compat.v1.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.gpu_options.visible_device_list = b"0,1,2"
-    with tf.compat.v1.Session(config=config,graph=tf.Graph()) as sess:
-      self.context = tf.compat.v1.placeholder(tf.int32, [1, None])
-      print(f'context = {self.context}')
-      output = sample_sequence(
-          hparams=self.hparams, length=self.length,
-          context=self.context,
-          batch_size=1,
-          temperature=self.temperature, top_k=self.top_k, top_p=self.top_p
-      )
-      print(f'output = {output}')
-      all_vars = tf.all_variables()
-      var_to_restore = []
-      for num, var1 in enumerate(all_vars):
-        if var1.name != 'model/wte:0':
-          var_to_restore.append(var1)
-      saver = tf.train.Saver(var_to_restore)
-      ckpt = tf.train.latest_checkpoint(self.model)
-      # print(type(ckpt)) # -> string
-      saver.restore(sess, ckpt)
-      saver = tf.train.Saver()
-      # saver.save(sess, '/src/gpt2-japanese/my-model', global_step=0)
-
-      for i in range(args.num_generate):
-        print(self.generate_one(sess, output))
-        if i < args.num_generate-1:
-            print('========')
-
 if __name__ == '__main__':
-  tokenizer = GPT2Tokenizer.from_pretrained('ja-117M')
-  # fine_tune = fine_tune_test()
-  # fine_tune.fine_tune()
+  # tokenizer = GPT2Tokenizer.from_pretrained('ja-117M')
+  fine_tune = fine_tune_test()
+  fine_tune.fine_tune()
--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+import logging
+logging.getLogger().setLevel(logging.CRITICAL)
+
+import warnings
+warnings.filterwarnings('ignore')
 import os
 from tokenizers.models import BPE
 from tokenizers import Tokenizer
@@ -6,7 +11,24 @@ from tokenizers.normalizers import NFKC, Sequence
 from tokenizers.pre_tokenizers import ByteLevel
 from tokenizers.trainers import BpeTrainer
 
-from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPT2Config, GPT2Model, GPT2LMHeadModel, GPT2Tokenizer
+import torch
+import numpy as np
+import pandas as pd
+import sys
+import json
+sys.path.append('/src/gpt2-japanese')
+
+from encode_bpe import BPEEncoder_ja
+
+with open('ja-bpe.txt') as f:
+  bpe = f.read().split('\n')
+
+with open('emoji.json') as f:
+  emoji = json.loads(f.read())
+
+tokenizer = BPEEncoder_ja(bpe, emoji)
+# import replace
 
 # tokenizer = Tokenizer(BPE())
 # tokenizer.normalizer = Sequence([
@@ -19,18 +41,66 @@ from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
 # tokenizer.model = BPE('./tokenized_data/vocab.json', './tokenized_data/merges.txt')
 
 
-
-tokenizer = GPT2Tokenizer.from_pretrained('./tokenized_data')
-tokenizer.add_special_tokens({
-  "eos_token"                : "</s>",
-  "bos_token"                : "<s>",
-  "unk_token"                : "<unk>",
-  "pad_token"                : "<pad>",
-  "mask_token"               : "<mask>",
-  "additional_special_tokens": ["<company>", "<label>", "<category>", "<review>"]
-})
+# tokenizer = GPT2Tokenizer.from_pretrained('gpt2-pytorch-model')
+# tokenizer.add_special_tokens({
+#   "eos_token"                : "</s>",
+#   "bos_token"                : "<s>",
+#   "unk_token"                : "<unk>",
+#   "pad_token"                : "<pad>",
+#   "mask_token"               : "<mask>",
+#   "additional_special_tokens": ["<company>", "<label>", "<category>", "<review>"]
+# })
 encoding = tokenizer.encode("<company>[company_id]<label>良い点<category>給与水準<review>年に２回ボーナスがある。")
 print(encoding)
 
 decoded = tokenizer.decode(encoding)
 print(decoded)
+
+model = GPT2LMHeadModel.from_pretrained('gpt2-pytorch-model-medium')
+model.resize_token_embeddings(len(tokenizer))
+
+model.eval()
+
+device = 'cpu'
+
+def choose_from_top(probs, n=5):
+  ind = np.argpartition(probs, -n)[-n:]
+  top_prob = probs[ind]
+  top_prob = top_prob / np.sum(top_prob) # Normalize
+  choice = np.random.choice(n, 1, p = top_prob)
+  token_id = ind[choice][0]
+  return int(token_id)
+
+seq_num = 0
+with torch.no_grad():
+  for idx in range(3):
+    print('=' * 5 + 'start' + '=' * 5)
+    finished = False
+    cur_ids = torch.tensor(tokenizer.encode('AIとは')).unsqueeze(0).to(device)
+
+    for i in range(100):
+      outputs = model(cur_ids, labels=cur_ids)
+      loss, logits = outputs[:2]
+      softmax_logits = torch.softmax(logits[0,-1], dim=0)
+      if i < 3:
+        n = 20
+      else:
+        n = 3
+      next_token_id = choose_from_top(softmax_logits.to('cpu').numpy(), n=n)
+      # print(next_token_id)
+      cur_ids = torch.cat([cur_ids, torch.ones((1,1)).long().to(device) * next_token_id], dim = 1)
+
+
+      if next_token_id in tokenizer.encode('<|endoftext|>'):
+        finished = True
+        break
+
+    # print(cur_ids.shape)
+    # if finished:
+    # print(cur_ids.tolist()[0])
+    seq_num = seq_num + 1
+    output_list = list(cur_ids.squeeze().to('cpu').numpy())
+    # output_list = cur_ids.tolist()[0]
+    output_text = tokenizer.decode(output_list)
+    output_text = output_text.replace(' ','')
+    print(output_text)
